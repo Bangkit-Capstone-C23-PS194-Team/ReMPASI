265023c69a3aa796cea64103a145d656c1e0dba4
zxc1520 <afifghulam21@gmail.com>
2023-05-18 06:40:42 +0700

    deleting experimental folder

--- cc-stuff/index.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-const server = http
-  .createServer((req, res) => {
-    res.setHeader("Content-Type", "text/html");
-    res.statusCode = 200;
-    res.end("Howdy");
-  })
-  .listen(3000);
